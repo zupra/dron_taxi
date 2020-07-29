b7c36d362a2a6d5c2b75598c70442a4a68731424
Dima Zubkov <dima5447298@gmail.com>
2020-07-29 20:12:17 +0300

    +

--- pages/config-users.vue
+++ pages/config-users.vue
@@ -10,50 +10,62 @@
       
 
     .Main
+      h1 Мой профиль
+
+      .Main_body
+
+        .flex
+
+          div
+            img(
+              style="width: 120px;"
+              :src="require(`~/static/img/clear-prof.png`)"
+            )
 
 
-      label Фамилия
-        input(
-          placeholder="Фамилия"
-        )
+          .ml-4
 
-      label Имя
-        input(
-          placeholder="Имя"
-        )
+            label Фамилия
+              input(
+                placeholder="Фамилия"
+              )
+
+            label Имя
+              input(
+                placeholder="Имя"
+              )
+
+            label Отчество
+              input(
+                placeholder="Отчество"
+              )
+
+          .ml-4
+            label Email
+              input(
+                placeholder="Email"
+              )
+
+            label Телефон
+              input(
+                placeholder="Телефон"
+              )
 
 
-      label Отчество
-        input(
-          placeholder="Отчество"
-        )
+            label Пароль
+              input(
+                placeholder="Пароль"
+              )
 
+            label Подтверждение пароля
+              input(
+                placeholder="Подтверждение пароля"
+              )
 
-      label Email
-        input(
-          placeholder="Email"
-        )
-
-      label Телефон
-        input(
-          placeholder="Телефон"
-        )
-
-
-      label Пароль
-        input(
-          placeholder="Пароль"
-        )
-
-      label Подтверждение пароля
-        input(
-          placeholder="Подтверждение пароля"
-        )
-
-      br
-      br
-      button.btn_green Сохранить
-      | &emsp;
-      button.btn_green Отмена
+        br
+        br
+        button.btn_green Сохранить
+        | &emsp;
+        button.btn_green Отмена
 
 </template>
